1ea64a289c181e743f5a9d161111b97d8133de01
ruizgerard8-cpu <ruizgerard8@gmail.com>
2026-05-30 18:26:40 +0200

    Add files via upload

---
+++ Features.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero.jsx'
+import Features from '../components/Features.jsx'
+import ProductSection from '../components/ProductSection.jsx'
+import Contact from '../components/Contact.jsx'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <ProductSection />
+      <Contact />
+    </>
+  )
+}
