e17eee0782b6ec9dedd9b2b147fd4cdd9fc8d170
ruizgerard8-cpu <ruizgerard8@gmail.com>
2026-05-30 18:57:29 +0200

    Add Features component with sustainable coffee info

---
+++ Features.jsx
@@ -0,0 +1,9 @@
+export default function Features() {
+  return (
+    <div>
+      <h2>Cultivo sostenible</h2>
+      <p>Café de especialidad</p>
+      <p>Origen colombiano</p>
+    </div>
+  );
+}
