96248ecb90fa3f8c86aa23cfb248b0d9f5e9aeec
ruizgerard8-cpu <ruizgerard8@gmail.com>
2026-05-30 18:59:56 +0200

    Delete Features.jsx

--- Features.jsx
+++
@@ -1,9 +0,0 @@
-export default function Features() {
-  return (
-    <div>
-      <h2>Cultivo sostenible</h2>
-      <p>Café de especialidad</p>
-      <p>Origen colombiano</p>
-    </div>
-  );
-}
